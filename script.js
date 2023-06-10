21b389d204d5b13a4b7d344ceb7a325e0b9953f8
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-10 12:39:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let arr = shortHex.split("");
+
+	let arr[1] = arr[1]+arr[1];
+	let arr[2] = arr[2]+arr[2];
+	let arr[3] = arr[3]+arr[3];
+
+	return arr.join("");
 };
 
 // Do not change the code below.
