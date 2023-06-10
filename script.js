87ff2d17c2d8779a78adc022f52f2916daa4ad88
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-10 12:45:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const extendHex = (shortHex) => {
   // write your code here
 	let arr = shortHex.split("");
 
+	
+	if (arr[0]!='#') {
+		arr[0] = arr[0]+arr[0];
+		arr[1] = arr[1]+arr[1];
+		arr[2] = arr[2]+arr[2];
+
+		return '#'+arr.join("");
+	}
+
 	arr[1] = arr[1]+arr[1];
 	arr[2] = arr[2]+arr[2];
 	arr[3] = arr[3]+arr[3];
-
 	return arr.join("");
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+const shortHex = promt("Enter Short Hex.");
 alert(extendHex(shortHex));
