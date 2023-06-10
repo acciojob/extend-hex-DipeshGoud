ceb7a40b8a374f00933d55f2a317a159ef2fc93e
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-10 12:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,5 +18,5 @@ const extendHex = (shortHex) => {
 };
 
 // Do not change the code below.
-const shortHex = promt("Enter Short Hex.");
+const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
